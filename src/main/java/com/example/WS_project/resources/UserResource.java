f6f286814c470021b1c73b787d650dea311b7f04
Edvaldo Neto <netoedvaldo98@gmail.com>
2026-07-01 20:27:38 -0300

    user entity and resource

--- src/main/java/com/example/WS_project/resources/UserResource.java
+++ src/main/java/com/example/WS_project/resources/UserResource.java
@@ -1,5 +1,19 @@
 package com.example.WS_project.resources;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.WS_project.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
 public class UserResource {
     
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        User u = new User(1L, "Maria", "maria@gmail.com", "988888888", "123456");
+        return ResponseEntity.ok().body(u);  
+    }
 }
