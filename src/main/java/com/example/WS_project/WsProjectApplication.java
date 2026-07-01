cc2496fe988d10e60234819422fb025e77138560
Edvaldo Neto <netoedvaldo98@gmail.com>
2026-07-01 19:52:53 -0300

    Project Created

---
+++ src/main/java/com/example/WS_project/WsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.WS_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/WS_project/WsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WS_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
