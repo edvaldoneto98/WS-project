2bec3b987c9c7e7c557b7c7aa414e2535f390e17
Edvaldo <netoedvaldo98@gmail.com>
2026-07-13 06:56:17 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/example/WS_project/entities/Category.java
+++ src/main/java/com/example/WS_project/entities/Category.java
@@ -8,6 +8,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -20,6 +21,7 @@ public class Category implements Serializable {
     private Long id;
     private String name;
 
+    @ManyToMany(mappedBy = "categories")
     private Set<Product> products = new HashSet<>();
 
     public Category() {
--- src/main/java/com/example/WS_project/entities/Product.java
+++ src/main/java/com/example/WS_project/entities/Product.java
@@ -4,10 +4,15 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -23,6 +28,9 @@ public class Product implements Serializable {
     private Double price;
     private String imgUrl;
 
+    @JsonIgnore
+    @ManyToMany
+    @JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id")) // The @JoinColumn annotation is already correctly applied to inverseJoinColumns.
     private Set<Category> categories = new HashSet<>();
 
     public Product() {
